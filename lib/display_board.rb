5687a7d678435abfb34c95ee778ac3c9b93d412c
Jae Yong Kim <kim900180@gmail.com>
2018-07-15 19:48:58 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  return "   |   |   
-  -----------
-     |   |   
-  -----------
-     |   |   "
+  row_1 = "   |   |   "
+  row_2 = "-----------"
+  row_3 = "   |   |   "
+  row_4 = "-----------"
+  row_5 = "   |   |   "
+  return row_1 + row_2 + row_3 + row_4 + row_5
 end
