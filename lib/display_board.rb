4edd2db9bcaf5dd3c6a1f7dd2b3d4242f09bfbc1
Jae Yong Kim <kim900180@gmail.com>
2018-07-15 19:50:52 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,6 @@ def display_board
   row_3 = "   |   |   "
   row_4 = "-----------"
   row_5 = "   |   |   "
-  board = row_1 + "/n" + row_2 + "/n" + row_3 + "/n" + row_4 + "/n" + row_5
+  board = row_1 + "\n" + row_2 + "\n" + row_3 + "\n" + row_4 + "\n" + row_5
   puts board
 end
